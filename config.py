bca3bf08e1a75d3945b4f531fbee592a055d2780
zhaolu1206 <30510452+zhaolu1206@users.noreply.github.com>
2024-08-01 16:49:28 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('config.json', 'r', encoding='utf-8') as f:
-    config = json.load(f)
-
-ADMIN_WECHAT_ID = config.get('ADMIN_WECHAT_ID')
-ADMIN_WECHAT_NICKNAME = config.get('ADMIN_WECHAT_NICKNAME')
