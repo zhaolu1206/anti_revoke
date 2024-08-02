443065fa6ba1e7554b0d989165e5a58f02c47d75
zhaolu1206 <30510452+zhaolu1206@users.noreply.github.com>
2024-08-02 17:02:56 +0800

    Update anti_revoke.py

--- anti_revoke.py
+++ anti_revoke.py
@@ -1,14 +1,30 @@
 import itchat
 from itchat.content import TEXT, NOTE
+from plugins import register, Plugin
+from bridge.context import ContextType, EventContext
+from bridge.reply import Reply, ReplyType
+from bridge.event import Event, EventAction
+import logging
 
-@itchat.msg_register([TEXT, NOTE], isGroupChat=True)
-def handle_revoke_msg(msg):
-    if msg.type == 'Note' and '撤回了一条消息' in msg.text:
-        revoked_msg_id = msg.text.split('msgid=')[1].split('&')[0]
-        revoked_msg = itchat.search_chatrooms(userName=msg.fromUserName).get(revoked_msg_id)
-        if revoked_msg:
-            admin = itchat.search_friends(nickName='老赵')[0]['UserName']
-            itchat.send(f"检测到撤回消息：{revoked_msg['Text']}", toUserName=admin)
+logger = logging.getLogger(__name__)
 
-itchat.auto_login(hotReload=True)
-itchat.run()
+@register(name="AntiRevoke", desc="A plugin to prevent message revocation", version="0.1", author="your_name", desire_priority= -1)
+class AntiRevoke(Plugin):
+    def __init__(self):
+        super().__init__()
+        self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
+        logger.info("[AntiRevoke] inited")
+
+    def on_handle_context(self, e_context: EventContext):
+        if e_context['context'].type != ContextType.TEXT and e_context['context'].type != ContextType.NOTE:
+            return
+        msg = e_context['context']['msg']
+        if e_context['context'].type == ContextType.NOTE and '撤回了一条消息' in msg.text:
+            revoked_msg_id = msg.text.split('msgid=')[1].split('&')[0]
+            revoked_msg = itchat.search_chatrooms(userName=msg.fromUserName).get(revoked_msg_id)
+            if revoked_msg:
+                admin = itchat.search_friends(nickName='老赵')[0]['UserName']
+                itchat.send(f"检测到撤回消息：{revoked_msg['Text']}", toUserName=admin)
+                e_context.action = EventAction.BREAK_PASS
+        else:
+            e_context.action = EventAction.CONTINUE
